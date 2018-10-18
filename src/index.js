1e70e04b997ba562f9a69ee6fcb12d4937a08147
Daniel Parsons <danielparsons3133382@gmail.com>
2018-10-18 18:36:09 +1100

    End of Section 7

--- src/index.js
+++ src/index.js
@@ -2,14 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom'
 import 'semantic-ui-css/semantic.min.css';
+import { Provider } from 'react-redux'
 import './index.css';
 import App from './app/layout/App';
 import * as serviceWorker from './serviceWorker';
+import { configureStore } from './app/store/configureStore';
+
+const store = configureStore();
 
 ReactDOM.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
+    <Provider store={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>
    , document.getElementById('root'));
 
 serviceWorker.unregister();
