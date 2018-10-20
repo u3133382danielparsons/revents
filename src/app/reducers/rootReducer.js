08207e57ba0f5cffeb9f7852ef75e0a5a039e376
Daniel Parsons <danielparsons3133382@gmail.com>
2018-10-21 02:23:45 +1100

    End of Section 12

--- src/app/reducers/rootReducer.js
+++ src/app/reducers/rootReducer.js
@@ -4,14 +4,15 @@ import testReducer from '../../features/testarea/testReducer';
 import eventReducer from '../../features/event/eventReducer'
 import modalsReducer from '../../features/modals/modalReducer'
 import authReducer from '../../features/auth/authReducer'
+import asyncReducer from '../../features/async/asyncReducer'
 
 const rootReducer = combineReducers({
   test: testReducer,
   events: eventReducer,
   form: FormReducer,
   modals: modalsReducer,
-  auth: authReducer
-
+  auth: authReducer,
+  async: asyncReducer
 })
 
 export default rootReducer
--- src/features/testarea/TestComponent.jsx
+++ src/features/testarea/TestComponent.jsx
@@ -6,16 +6,17 @@ import PlacesAutocomplete, {
   geocodeByAddress,
   getLatLng
 } from 'react-places-autocomplete';
-import { incrementCounter, decrementCounter } from './testActions';
+import { incrementAsync, decrementAsync } from './testActions';
 import { openModal } from '../modals/modalActions'
 
 const mapState = state => ({
-  data: state.test.data
+  data: state.test.data,
+  loading: state.test.loading
 });
 
 const actions = {
-  incrementCounter,
-  decrementCounter,
+  incrementAsync,
+  decrementAsync,
   openModal
 };
 
@@ -55,7 +56,7 @@ class TestComponent extends Component {
       onChange: this.onChange
     };
 
-    const { incrementCounter, decrementCounter, data, openModal } = this.props;
+    const { incrementAsync, decrementAsync, data, openModal, loading } = this.props;
     return (
       <div>
         <Script
@@ -64,8 +65,8 @@ class TestComponent extends Component {
         />
         <h1>Test Area</h1>
         <h3>The answer is: {data}</h3>
-        <Button onClick={incrementCounter} color="green" content="Increment" />
-        <Button onClick={decrementCounter} color="red" content="Decrement" />
+        <Button loading={loading} onClick={incrementAsync} color="green" content="Increment" />
+        <Button loading={loading} onClick={decrementAsync} color="red" content="Decrement" />
         <Button onClick={() => openModal('TestModal', {data: 43})} color="teal" content="Open Modal" />
         <br />
         <br />
